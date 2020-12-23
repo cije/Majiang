81eb197296b0cf90179cc6f4fcdbd77da6cdf560
cije <an37500@163.com>
2020-12-23 21:57:18 +0800

    th login  config

--- src/main/java/com/ce/majiang/controller/AuthorizeController.java
+++ src/main/java/com/ce/majiang/controller/AuthorizeController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author c__e
@@ -31,7 +34,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO()
                 .setClient_id(client_id)
                 .setClient_secret(client_secret)
@@ -39,7 +43,13 @@ public class AuthorizeController {
                 .setRedirect_uri(redirect_uri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        if (!ObjectUtils.isEmpty(user)) {
+            // 登陆成功 写Cookie和Session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            // 登陆失败，重新登陆
+            return "redirect:/";
+        }
     }
 }
